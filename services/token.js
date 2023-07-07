fbbc3f7dfe3d139d0f9bfd3e652ab5c839f4e65e
NIKHIL RANJAN KUMAR <88980648+niku-19@users.noreply.github.com>
2023-07-07 19:19:49 +0530

    jwt token fix

--- services/token.js
+++ services/token.js
@@ -1,10 +1,10 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const generateToken = (user) => {
-  const token = jwt.sign(user, process.env.JWT_SECRET,{
-    expiresIn: '1d',
-  })
+  const token = jwt.sign(user, process.env.JWT_SECRET, {
+    expiresIn: "365d",
+  });
   return token;
-}
+};
 
 export default generateToken;
